165413c40968fd92b4304724582c85d8634b0f06
Labrat <89636989+xq34env1@users.noreply.github.com>
2025-01-09 10:19:13 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -15,31 +15,49 @@ function OutputBMI()
 {
     var weight = document.getElementById("InputWeight").value;
     var height = document.getElementById("InputHeight").value;
-    var bmi = (weight * 703) / (height * height);
-    var fullstring = "Your Total BMI Is " + bmi.toFixed(2);
-    document.getElementById("bmicalculated").textContent = fullstring;
+	if(weight <= 0 || weight > 2000)
+	{
+			alert("Inputs Are Either In Negatives Or Above Human Limits.")
+	}
+	
+	else{
+		var bmi = (weight * 703) / (height * height);
+		var fullstring = "Your Total BMI Is " + bmi.toFixed(2);
+		document.getElementById("bmicalculated").textContent = fullstring;
 
-    if(bmi < 18.50)
-    {
-        document.getElementById("bmitype").style.color = "Yellow"
-        document.getElementById("bmitype").textContent = underweight;
-    }
-
-    if(bmi > 18.51 && bmi < 24.90)
-    {
-        document.getElementById("bmitype").style.color = "Green"
-        document.getElementById("bmitype").textContent = healthy;
-    }
-
-    if(bmi > 25 && bmi < 29.90)
-    {
-        document.getElementById("bmitype").style.color = "Orange"
-        document.getElementById("bmitype").textContent = overweight;
-    }
-
-    if(bmi > 30)
-    {
-        document.getElementById("bmitype").style.color = "Red"
-        document.getElementById("bmitype").textContent = obese;
-    }
+		switch(true)
+		{
+			case bmi < 18.50 && bmi > 0:
+				document.getElementById("bmitype").style.color = "Yellow"
+				document.getElementById("bmitype").textContent = underweight;
+				break;
+				
+			case bmi > 17.51 && bmi < 24.90:
+				document.getElementById("bmitype").style.color = "Green"
+				document.getElementById("bmitype").textContent = healthy;
+				break;
+				
+			case bmi > 25 && bmi < 29.90:
+				document.getElementById("bmitype").style.color = "Orange"
+				document.getElementById("bmitype").textContent = overweight;
+				break;
+			
+			case bmi > 30 && bmi < 90:
+				document.getElementById("bmitype").style.color = "Red"
+				document.getElementById("bmitype").textContent = obese;
+				break;
+				
+			case bmi <= 0:
+				alert("BMI Goes Below Limits, Seek Medical Attention Or Enter The Correct Inputs");
+				break;
+				
+			case bmi > 90:
+				alert("BMI Goes Above Possibly Healthy Limits, Seek Medical Attention Or Enter Correct Inputs");
+				break;
+				
+			default:
+				break;
+				
+		}
+	}
 }
